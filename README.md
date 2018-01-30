76b42d7f1be3b9a70e556735cdc23a1da4d13b9c
Matt Leach <contact@mattleachdigital.com>
2018-01-30 19:03:33 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Boilerplate
+
+Rather than using the Create React App, I want a more customisable starting point for my React apps. I will be adding in testing once I decide on which testing library to go with.
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+        <p>This works!</p>
+        <span>How's this</span>
+      </div>
+    );
+  }
+}
+
+export default App;
